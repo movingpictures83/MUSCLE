697e9f809e62f6ffdb78bef742104c947470e8d3
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-03-21 13:19:54 -0400

    Changed input FASTAS

--- MUSCLEPlugin.cpp
+++ MUSCLEPlugin.cpp
@@ -18,11 +18,13 @@ void MUSCLEPlugin::run() {
 }
 
 void MUSCLEPlugin::output(std::string file) {
-std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH};";
+
+//std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH};";
 //align_seqs.py -i HMP_MOCK.v35.fasta -m muscle -o output
-command += "align_seqs.py -i "+inputfile+" -m muscle -o "+file+"; cp "+file+"/*_aligned.fasta "+PluginManager::prefix();
-command += "; export PYTHONPATH=OLDPATH" ;
- system(command.c_str());
+//command += "align_seqs.py -i "+inputfile+" -m muscle -o "+file+"; cp "+file+"/*_aligned.fasta "+PluginManager::prefix();
+//command += "; export PYTHONPATH=OLDPATH" ;
+	std::string command = "muscle -in "+inputfile+" -out "+file;
+       	system(command.c_str());
 }
 
 PluginProxy<MUSCLEPlugin> MUSCLEPluginProxy = PluginProxy<MUSCLEPlugin>("MUSCLE", PluginManager::getInstance());
